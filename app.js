d74d4caa00469fdab315cea38d7a4921f24262af
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-09 15:58:44 +0200

    feat: import express and add port

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('sono una webapp')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+app.get('/', (req, res) => {
+    res.send('Homepage');
+})
+
+app.listen(port, () => {
+    console.log(`webapp listening at port ${port}`);
+})
