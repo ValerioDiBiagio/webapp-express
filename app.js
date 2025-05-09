3ad19bba15b412fb10b23b80406e9e698dfd2e91
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-09 16:30:33 +0200

    feat: add controller and router

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+const moviesRouter = require('./routers/movies')
 
 app.get('/', (req, res) => {
     res.send('Homepage');
 })
+
+app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
     console.log(`webapp listening at port ${port}`);
---
+++ controllers/movieController.js
@@ -0,0 +1,10 @@
+export function index(req, res) {
+    res.send('All movies');
+
+}
+
+
+export function show(req, res) {
+    res.send('Single movies');
+
+}
