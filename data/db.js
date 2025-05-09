ea720c778e5be077a501cae0abcf5fecc96edbea
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-09 17:27:08 +0200

    feat: add env

--- data/db.js
+++ data/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'db_movies'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
