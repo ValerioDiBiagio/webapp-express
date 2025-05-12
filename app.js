6eb1b0665bacdc38468c880f281a7dca5f4aa8d5
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-12 18:00:19 +0200

    feat: add cors

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const cors = require('cors');
 
 const moviesRouter = require('./routers/movies');
 
 const notFound = require('./middlewares/notFound');
 const errorsHandler = require('./middlewares/handleErrors');
+
+app.use(cors({
+    origin: process.env.FE_APP
+}));
 
 app.use(express.static('public'));
 
